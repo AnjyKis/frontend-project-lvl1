99b11fcb99bf9e2662bd180f4f81403505a04fc2
anjykis <anjykis1995@gmail.com>
2022-04-09 18:17:02 +0300

    Исправлен предыдущий шаг

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,31 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import brainEven from '../src/even.js';
 
-const check = (guess, answer) => {
-  if (guess !== 'yes' && guess !== 'no') {
-    return false;
-  }
-  const guessIsEven = guess === 'yes';
-  const result = answer === guessIsEven;
-  return result;
-};
-
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const value = Math.floor(Math.random() * 100);
-    const isEven = value % 2 === 0;
-    console.log(`Question: ${value}`);
-    const guess = readlineSync.question('Your answer: ');
-    const isRight = check(guess, isEven);
-    const result = isRight ? 'Correct!' : `'${guess}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, Bill!`;
-    console.log(result);
-    if (!isRight) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
+brainEven();
