f6369fcfabfefca491ccd5d069d7177516975aed
anjykis <anjykis1995@gmail.com>
2022-04-09 21:26:59 +0300

    мелкий фикс

--- src/even.js
+++ src/even.js
@@ -3,7 +3,7 @@ import engine from './index.js';
 
 const question = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const f = () => {
+export default () => {
     const condition = () => {
 
         const randomNumber = Math.floor(Math.random() * 100); 
@@ -17,34 +17,4 @@ const f = () => {
     };
 
     engine(question, 3, condition);
-}
-
-f()
-// import cli from './cli.js';
-
-// const check = (guess, answer) => {
-//   if (guess !== 'yes' && guess !== 'no') {
-//     return false;
-//   }
-//   const guessIsEven = guess === 'yes';
-//   const result = answer === guessIsEven;
-//   return result;
-// };
-
-// export default () => {
-//   const name = cli();
-//   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-//   for (let i = 0; i < 3; i += 1) {
-//     const value = Math.floor(Math.random() * 100);
-//     const isEven = value % 2 === 0;
-//     console.log(`Question: ${value}`);
-//     const guess = readlineSync.question('Your answer: ');
-//     const isRight = check(guess, isEven);
-//     const result = isRight ? 'Correct!' : `'${guess}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${name}`;
-//     console.log(result);
-//     if (!isRight) {
-//       return;
-//     }
-//   }
-//   console.log(`Congratulations, ${name}!`);
-// };
+};
